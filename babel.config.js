0c7b4ff35355dce0702a3707406cdb489b4b27eb
EdoardoUlrichProverbio <proverbio.1658119@studenti.uniroma1.it>
2026-04-25 11:12:37 +0200

    fix bundling

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      require.resolve("babel-preset-expo", {
+        paths: [require.resolve("expo/package.json")],
+      }),
+    ],
   };
 };
